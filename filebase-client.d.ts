dd15f4a1a4578d1b1431cd274d1f01f7037de538
@lpha B <rsuman7868@gmail.com>
2024-10-13 02:52:52 +0530

    filebase fix error

---
+++ filebase-client.d.ts
@@ -0,0 +1,8 @@
+declare module '@filebase/client' {
+    export class FilebaseClient {
+      storeBlob: any;
+      constructor(config: any);
+      // Add any methods or properties you need from the client here
+    }
+  }
+  
